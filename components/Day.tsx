852fa6dd4191b6bf385f9e0a935813c1616e2774
Sondre Nilsen <sondre@eons.io>
2021-11-19 16:49:52 +0100

    Hide old events below button

--- components/Day.tsx
+++ components/Day.tsx
@@ -1,4 +1,4 @@
-import { Activity, DayPlan, Entry } from "../lib/data";
+import { Activity, DayPlan, Entry, PlainEntry } from "../lib/data";
 import {
   MdCoffee,
   MdDinnerDining,
@@ -6,6 +6,8 @@ import {
   MdAccessAlarm,
   MdCheck,
   MdOutlineCheckCircleOutline,
+  MdExpandMore,
+  MdExpandLess,
 } from "react-icons/md";
 import {
   formatDistance,
@@ -16,6 +18,7 @@ import {
   format,
 } from "date-fns";
 import { nb } from "date-fns/locale";
+import { useState } from "react";
 
 const getIcon = (activity: Activity): JSX.Element => {
   switch (activity) {
@@ -44,11 +47,18 @@ const formatDate = (date: Date): string => {
   return format(date, "HH:mm");
 };
 
-interface Props {
-  day: DayPlan;
+interface ApiEntry extends PlainEntry {
+  time: string;
 }
 
-const DayEntry = ({ entry }: { entry: Entry }): JSX.Element => {
+interface Props {
+  day: {
+    name: string;
+    activities: Array<ApiEntry>;
+  };
+}
+
+const DayEntry = ({ entry }: { entry: ApiEntry }): JSX.Element => {
   const time = parseISO(entry.time as string);
   const isEntryAfter = isAfter(time, new Date());
 
@@ -92,33 +102,90 @@ const DayEntry = ({ entry }: { entry: Entry }): JSX.Element => {
   );
 };
 
+interface EventListProps {
+  events: Array<ApiEntry>;
+}
+
+const EventList = ({ events }: EventListProps): JSX.Element => {
+  return (
+    <ul role="list" className="-mb-8">
+      {events.map((plan, planIdx) => {
+        const nextEntry = events[planIdx + 1];
+        return (
+          <li key={planIdx}>
+            <div className="relative pb-8">
+              {nextEntry && isAfter(parseISO(nextEntry.time), new Date()) ? (
+                <span
+                  className="absolute top-5 left-5 -ml-px h-full w-1 bg-gray-400"
+                  aria-hidden="true"
+                />
+              ) : planIdx !== events.length - 1 ? (
+                <span
+                  className="absolute top-5 left-5 -ml-px h-full w-1 bg-gray-200"
+                  aria-hidden="true"
+                />
+              ) : null}
+              <DayEntry entry={plan} key={planIdx} />
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+interface ShowProps {
+  show: boolean;
+  setShow: (show: boolean) => void;
+}
+
+const ShowMore = ({ show, setShow }: ShowProps): JSX.Element => (
+  <div className="relative mt-8">
+    <div className="absolute inset-0 flex items-center" aria-hidden="true">
+      <div className="w-full border-t border-gray-300" />
+    </div>
+    <div className="relative flex justify-center">
+      <button
+        onClick={() => setShow(!show)}
+        type="button"
+        className="inline-flex items-center shadow-sm px-4 py-1.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        {show ? (
+          <MdExpandLess
+            className="-ml-1.5 mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        ) : (
+          <MdExpandMore
+            className="-ml-1.5 mr-1 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        )}
+        <span>Previously...</span>
+      </button>
+    </div>
+  </div>
+);
+
 export const Day = ({ day }: Props): JSX.Element => {
+  const [show, setShow] = useState(false);
+  let after = day.activities.filter((it) =>
+    isBefore(parseISO(it.time), new Date())
+  );
+  const before = day.activities.filter((it) =>
+    isAfter(parseISO(it.time), new Date())
+  );
+
+  if (after.length > 0) {
+    before.push(after[0]);
+    after = after.slice(1);
+  }
+
   return (
     <div className="flow-root">
-      <ul role="list" className="-mb-8">
-        {day.activities.map((plan, planIdx) => {
-          const nextEntry = day.activities[planIdx + 1];
-          return (
-            <li key={planIdx}>
-              <div className="relative pb-8">
-                {nextEntry &&
-                isAfter(parseISO(nextEntry.time as string), new Date()) ? (
-                  <span
-                    className="absolute top-5 left-5 -ml-px h-full w-1 bg-gray-400"
-                    aria-hidden="true"
-                  />
-                ) : planIdx !== day.activities.length - 1 ? (
-                  <span
-                    className="absolute top-5 left-5 -ml-px h-full w-1 bg-gray-200"
-                    aria-hidden="true"
-                  />
-                ) : null}
-                <DayEntry entry={plan} key={planIdx} />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <EventList events={before} />
+      <ShowMore show={show} setShow={setShow} />
+      {show && <EventList events={after} />}
     </div>
   );
 };
