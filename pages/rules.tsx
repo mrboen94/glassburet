800aa8989fecd451fd4aba05c08ac353a85ecb2a
Mathias <mrboen94@gmail.com>
2022-02-07 19:08:38 +0100

    updated rules a bit

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -1,41 +1,78 @@
 export default function Rules() {
   return (
-    <div className="prose lg:prose-xl">
-      <h1>Del 1: Anmerkninger</h1>
+    <div className="prose prose-sm lg:prose-lg xl:prose-7xl">
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="px-3 bg-white text-lg font-medium text-gray-900">
+            Del 1: Anmerkninger
+          </span>
+        </div>
+      </div>
       <h3>§ 1.1 - Typer anmerkninger</h3>
       <ul>
         <li>
-          💪 Treningsanmerkning - Utebli fra trening Mandag, Onsdag og Fredag
-        </li>
-        <li>🛗 Heisanmerkning - Ved heisbruk.</li>
-        <li>☕ Søleanmerkning - Ved søling av farget væske i trappeoppgang.</li>
-        <li>
           🕙 Tidsanmerkning - Ved å komme på skolen etter klokken 10:00 på
           Tirsdag og Torsdag (gjelder ikke gyldig fravær som kan dokumenteres,
-          at man sitter på MMIV, HVL o.l.).
+          at man sitter på MMIV, HVL, o.l.).
         </li>
+        <li>
+          💪 Treningsanmerkning - Utebli fra trening Mandag, Onsdag og Fredag
+        </li>
+        <li>☕ Søleanmerkning - Ved søling av farget væske i trappeoppgang.</li>
       </ul>
-      <p>Ved unntak les § 3.1</p>
-      <h1> Del 2: Trening</h1>
+      <p>
+        <b>Ved unntak les § 3.1</b>
+      </p>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="px-3 bg-white text-lg font-medium text-gray-900">
+            Del 2: Trening
+          </span>
+        </div>
+      </div>
       <h3>§ 2.1 - Unntak fra treningsanmerkning</h3>
       <p>
-        Der er kun en type unntak fra treningsanmerkning (se Kapittel 2 -
-        sykemelding - § 2.2).
+        Der er kun én type unntak fra treningsanmerkning (se Del 3 - sykemelding
+        - § 2.2).
       </p>
       <h3>§ 2.2 - Innhenting av anmerkninger</h3>
       <p>
-        Med unntak av §2.2 så er eneste måten å unngå en treningsanmerkning å
+        Med unntak av §2.1 så er eneste måten å unngå en treningsanmerkning å
         trene den inn i etterkant av anmerkningens oppføring.
       </p>
       <h3>§ 2.3 - Juks ved treningsanmerkning</h3>
       <p>Et hvert forsøk på juks vil bli straffet med to nye anmerkninger.</p>
-      <h1> Del 3: Sykemelding</h1>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="px-3 bg-white text-lg font-medium text-gray-900">
+            Del 3: Sykemelding
+          </span>
+        </div>
+      </div>
       <h3>§ 3.1 - Sykemelding</h3>
       <p>
         Man kan søke i chatten om sykemelding om man føler seg guffen og dårlig,
         det blir så avgjort med en avstemning.
       </p>
-      <h1> Del 4: Master skriving (TBD)</h1>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-300" />
+        </div>
+        <div className="relative flex justify-center">
+          <span className="px-3 bg-white text-lg font-medium text-gray-900">
+            Del 4: Master skriving (TBD)
+          </span>
+        </div>
+      </div>
       <h3>§ 4.1 - Mengde</h3>
       <p>
         Fra og med 01.01.2022 vil det bli oppført anmerkning dersom man ikke har
